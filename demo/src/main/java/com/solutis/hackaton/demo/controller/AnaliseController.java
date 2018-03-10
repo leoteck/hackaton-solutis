2d63d5be2bca368832b871f8f0051bf28ad92845
Leonardo Rodrigues <leonardo.rodrigues@solutis.com.br>
2018-03-10 17:57:11 -0300

    ajuste

--- demo/src/main/java/com/solutis/hackaton/demo/controller/AnaliseController.java
+++ demo/src/main/java/com/solutis/hackaton/demo/controller/AnaliseController.java
@@ -2,7 +2,7 @@ package com.solutis.hackaton.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ public class AnaliseController {
 	@Autowired
 	private AnaliseService analiseService;
 	
-	@PostMapping(path = "/analisar")
+	@GetMapping(path = "/analisar")
 	public ResponseEntity<String> analisar() {
 		return ResponseEntity.ok(analiseService.montarConteudo());
 	}
